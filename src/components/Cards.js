e0a701ee0ed7f5bbf3db59e22433f3155815dd44
motajessica <motajessicaa@gmail.com>
2023-04-20 11:27:50 +1200

    Change cards quantity

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -32,6 +32,33 @@ function Cards () {
         </div>
       </div>
 
+      <div className="card m-2">
+        <img src="..." className="card-img-top" ></img>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#">Go somewhere</a>
+        </div>
+      </div>
+
+      <div className="card m-2">
+        <img src="..." className="card-img-top" ></img>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#">Go somewhere</a>
+        </div>
+      </div>
+
+      <div className="card m-2">
+        <img src="..." className="card-img-top" ></img>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#">Go somewhere</a>
+        </div>
+      </div>
+
     </div>
      
 
