3a8d49dae3ebd739bbec812fe5e34414463f8630
motajessica <motajessicaa@gmail.com>
2023-04-20 11:38:00 +1200

    Fix footer position

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
   const year = new Date().getFullYear()
   return (
     <footer>
-      <div className="footer w-100 text-center pt-4 pb-4">
+      <div class="footer w-100 text-center bg-color pt-4 pb-3">
         
         <footer>Recipes App{year} </footer>
       </div>
