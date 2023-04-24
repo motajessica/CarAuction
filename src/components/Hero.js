ff28ff3a9a89d8a5e857f95a1b68134b80695a8a
motajessica <motajessicaa@gmail.com>
2023-04-25 10:37:04 +1200

    WIP - Changes on hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,37 +1,46 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-function Hero () {
+function Hero() {
   return (
-
-      <div className='img-hero mb-5 p-5 text-center bg-image' style={{ backgroundImage: "url('/images/cooking-1.jpg')", height:400 }}>
-        
-        <div className="row">
-          <div className="col-lg-6 mb-5" id="title" >
-            <h2 className="text-white mb-3">Unleash your inner chef with our amazing & simple recipes.</h2>
-            <button type="button" class="btn btn-dark btn-lg download-button mb-3" ><i class="fab fa-apple me-1"></i>Download</button>
-            <button type="button" class="btn btn-outline-light btn-lg download-button mb-3"><i class="fab fa-google-play me-1"></i>Download</button>
-         </div>
-
-        <div className="col-lg-6 position-relative">
+    <div
+      className="img-hero mb-5 p-5 text-center bg-image"
+      style={{ backgroundImage: "url('/images/cooking-1.jpg')", height: 400 }}
+    >
+      <div className="row">
+        <div className="col-lg-6 mb-5">
+          <h2 className="text-white mb-3">
+            Unleash your inner chef with our amazing & simple recipes.
+          </h2>
+          <button
+            type="button"
+            class="btn btn-dark btn-lg download-button mb-3"
+          >
+            <i class="fab fa-apple me-1"></i>Download
+          </button>
+          <button
+            type="button"
+            class="btn btn-outline-light btn-lg download-button mb-3"
+          >
+            <i class="fab fa-google-play me-1"></i>Download
+          </button>
         </div>
 
+        <div className="col-lg-6 position-relative"></div>
+
         <div className="search mb-5">
-          <input 
+          <input
             className="mb-2 mx-2 mr-sm-2 "
             type="search"
-            placeholder='Search'  
+            placeholder="Search"
             onChange={() => {}}
           />
-        <button type="submit" className="btn my-2 my-sm-0 btn-sm btn-warning">Ok</button>
-      </div> 
-
+          <button type="submit" className="btn my-2 my-sm-0 btn-sm btn-warning">
+            Ok
+          </button>
+        </div>
       </div>
-    </div>     
-
-
+    </div>
   );
 }
 
