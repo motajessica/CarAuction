03ec352650c37b41b5a1c73750a04a4a5f7152e7
motajessica <motajessicaa@gmail.com>
2023-04-25 13:16:58 +1200

    Change layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,7 @@ function Hero() {
   return (
     <div
       className="img-hero mb-5 p-5 text-center bg-image"
-      style={{ backgroundImage: "url('/images/bread-1.jpg')", height: 500 }}
-    >
+      style={{ backgroundImage: "url('/images/bread-1.jpg')", height: 500 }}>
       <div className="row py-5">
         <div className="col-lg-6 mb-5">
           <h2 className="text-white mb-3">
@@ -14,20 +13,18 @@ function Hero() {
           </h2>
           <button
             type="button"
-            class="btn btn-dark btn-lg download-button mb-3"
-          >
+            class="btn btn-dark btn-lg download-button mb-3">
             <i class="fab fa-apple me-1"></i>Download
           </button>
           <button
             type="button"
-            class="btn btn-outline-light btn-lg download-button mb-3"
-          >
+            class="btn btn-outline-light btn-lg download-button mb-3">
             <i class="fab fa-google-play me-1"></i>Download
           </button>
         </div>
-
+       
         <div className="col-lg-6 position-relative"></div>
-
+        
       </div>
     </div>
   );
