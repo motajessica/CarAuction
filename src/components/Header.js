665cf9bedd68e727e03244a31f37bba638384597
motajessica <motajessicaa@gmail.com>
2023-04-26 08:59:39 +1200

    Changes on navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-warning px-4">
+    <Navbar expand="lg" className="navbar bg-warning px-4">
       <Navbar.Brand className="nav-brand" href="/">Recipes App</Navbar.Brand>
       <Navbar.Toggle aria-controls="nav" />
       <Navbar.Collapse id="nav">
@@ -12,7 +12,7 @@ function Header() {
           <Nav.Link href="/lists">HOME</Nav.Link>
           <Nav.Link href="/about">ABOUT US</Nav.Link>
           <Nav.Link href="/contact">CONTACT US</Nav.Link>
-          <Nav.Link class="item-login" href="/contact">LOGIN</Nav.Link>
+          <Nav.Link class="item-login btn btn-outline-dark" href="/contact">LOGIN</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
