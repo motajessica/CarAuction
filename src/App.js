e5467e3fbbd7fc770abf28e4a45ac6857f08737a
motajessica <motajessicaa@gmail.com>
2023-04-27 20:06:41 +1200

    Fix login button on navbar

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import Cards from "./components/Card.js";
 import Search from "./components/Search.js";
 import Hero from "./components/Hero.js";
 import "./App.css";
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-   <div className="App">
-   
-    <div>
-      <Header />
-      <Hero />
-    </div>
+    <div className="App">
+      <div>
+        <Header />
+        <Hero />
+      </div>
 
-    <div className="container">
-      <Search />
-    </div>
+      <div className="container">
+        <Search />
+      </div>
 
-    <div className="container">
-      <div className="d-flex flex-wrap justify-content-center">
-        {
-          ["quesadilla", "burger", "lasagna", "paella", "cake", "meal"].map(function(item) {
-            return <Cards imageSource={item}/>
-          })
-        }
+      <div className="container">
+        <div className="d-flex flex-wrap justify-content-center">
+          {["quesadilla", "burger", "lasagna", "paella", "cake", "meal"].map(
+            function (item) {
+              return <Cards imageSource={item} />;
+            }
+          )}
+        </div>
+      </div>
+
+      <div className="footer">
+        <Footer />
       </div>
     </div>
-      
-    <div className="footer">
-      <Footer />
-    </div>  
-
-  </div>
   );
-}
+};
 
 export default App;
