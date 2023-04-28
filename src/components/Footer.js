f75a46840ef2d7d07b7f0754976b59a42c2b91a0
motajessica <motajessicaa@gmail.com>
2023-04-28 19:25:54 +1200

    Improve footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 function Footer() {
   const year = new Date().getFullYear()
   return (
-    <footer className="footer">
-      <div class="footer w-100 text-center pt-3">
-        <footer>CookBook App © {year} </footer>
-      </div>
+    <footer className="footer py-3 my-4">
+    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+      <li className="nav-item">
+        <a href='#' className="nav-link px-2 text-muted"> Home</a>
+       </li>
+       <li className="nav-item">
+        <a href='#' className="nav-link px-2 text-muted"> Features</a>
+       </li>
+       <li className="nav-item">
+        <a href='#' className="nav-link px-2 text-muted"> FAQs</a>
+       </li>
+       <li className="nav-item">
+        <a href='#' className="nav-link px-2 text-muted"> Contact us</a>
+       </li>
+       <li className="nav-item">
+        <a href='#' className="nav-link px-2 text-muted"> About us</a>
+       </li>
+    </ul>
+    <p className="text-center text-muted ">CookBook App © {year} </p>
   </footer>
   );
 };
