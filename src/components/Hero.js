a2a581139deb24d56874aef3edb362fde8927e42
motajessica <motajessicaa@gmail.com>
2023-05-08 21:04:03 +1200

    Change color and remove image on hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,15 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Hero() {
   return (
+<div>
+
 
     <div
-    className="hero-background hero-img mb-5 p-5 text-left bg-image"
-    style={{ backgroundImage: "url('/images/bread.jpg')", height: 500 }}
+    className="hero-background mb-5 p-5 text-left"
+    style={{height: 600 }}
     >
       <div className="py-5 d-flex flex-column justify-content-center gap">
-        <h2 className="hero-title container">
-          Turners Car
-        </h2>
+        <h1 className="hero-title container">
+          Turners
+        </h1>
         <div className="hero-subtitle container">
           <h3 className="mt-5">
             We love buying cars
@@ -19,6 +21,15 @@ function Hero() {
         </div>
       </div>
     </div>
+
+
+
+
+
+
+</div>
+
+
     // <div className="hero-background">
     //   <div
     //   className="hero-img mb-5 p-5 bg-image"
